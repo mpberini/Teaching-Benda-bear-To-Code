7dfd797b4ed30cd74b51b2ed4d2cb4aea0909dc8
Madison Berini <mpberini@gmail.com>
2016-12-13 18:40:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-##This is your coding lesson, created by the one and only- MPBERINI!!!
-##Yay!!!
+/* This is your coding lesson, created by the one and only- MPBERINI!!!
+##Yay!!! */
+
+/* First off, this weird gray text is a comment. This is not viewable in your game and is only viewable to you! 
+See the slash and star that is at the beginning and end of this text? Well, that's how you make a comment in javascript! */
+
+'use strict'
+
+/* This line (the one above) is what starts off your game/ website! */
+/* Next, lets do a simple command! The "var" command! You know what a variable is in Algebra, right? Same thing! 
+It goes like this:  */
+
+var x = 5;
+
+/* See? Easy enough, right? Oh yea, and you see that semicolon at the end? That is necessary for your program! 
+This ends off what you typed so you can do the rest of the code! */
+
+/* Now lets use that var in the "if" command! */
